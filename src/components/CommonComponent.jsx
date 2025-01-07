4312136920d35a86ce23b74c49484789670ec489
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2025-01-07 16:23:37 +0200

    addcomit_16.20_07.01.2025
    Трохи змінено логіку підрахунку проценту вигоди, тепер підрахунок здійснюється в масиві pairs. Реалізовано це для того, щоб можна було в майбутньому створити кнопки для сортування від більшого до меншого відсотку доходу та приховання не вигідних пар. Коли розрахунок відбувається в дочірньому компоненті здійснити сортування стає неможливим.

--- src/components/CommonComponent.jsx
+++ src/components/CommonComponent.jsx
@@ -1,29 +1,8 @@
-import { calculateProfit } from "../utils/calculateProfit.js";
 
 export function CommonComponnt({
-  workingСapital,
-  pricesFirstCoin,
-  pricesSecondCoin,
-  pricesCoinToCoin,
   pairName,
+  profitInPercentage,
 }) {
-  // Перевірка наявності всіх необхідних даних для розрахунку
-  if (
-    !workingСapital ||
-    !pricesFirstCoin ||
-    !pricesSecondCoin ||
-    !pricesCoinToCoin
-  ) {
-    return <p>Не всі данні отримані</p>;
-  }
-
-  // Виконання розрахунків
-  const profitInPercentage = calculateProfit({
-    workingСapital,
-    pricesFirstCoin,
-    pricesCoinToCoin,
-    pricesSecondCoin,
-  });
 
   // Динамічний стиль для прибутку
   const profitStyle =
